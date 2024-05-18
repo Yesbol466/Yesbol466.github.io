9038aeb1179e6226b7eaaf31486b63a640fe1070
Yesbol Yerlan <72014866+Yesbol466@users.noreply.github.com>
2024-05-19 00:43:57 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,11 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Placeholder for any additional JavaScript you might want to add
